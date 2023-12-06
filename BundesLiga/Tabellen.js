a29096e7f0a2ea789b383f59bed7ae73e1e22c03
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2023-12-06 11:06:17 +0100

    API FERTIG

--- BundesLiga/Tabellen.js
+++ BundesLiga/Tabellen.js
@@ -1,7 +1,29 @@
-const loadGamesData = () => {
-    fetch("https://api.openligadb.de/getmatchdata/bl1/2023").then((result) => {
-        result.json().then((data) => {
-            console.loadGamesData
-        })
+function loadTable() {
+  fetch("https://api.openligadb.de/getbltable/öbl1/2023").then((res) =>
+    res.json().then((result) => {
+      insertTable(result);
     })
+  );
 }
+
+function insertTable(data) {
+  console.log("data", data);
+  const element = document.getElementById("table");
+
+  let output = "";
+  data.forEach((team) => {
+    output += `<div class="SecondHeader">
+                <div class="VereinRB">${team.teamName}</div>
+                <div class="GamesRB">${team.matches}</div>
+                <div class="SiegRB">${team.won}</div>
+                <div class="UnentschiedenRB">${team.draw}</div>
+                <div class="NiederlageRB">${team.lost}</div>
+                <div class="ToreRB">${team.goals}</div>
+                <div class="PunkteRB">${team.points}</div>
+              </div>`;
+  });
+
+  element.innerHTML = output;
+}
+
+const data = loadTable();
